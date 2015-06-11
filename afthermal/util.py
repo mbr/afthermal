3398a3e8b2a19d145ed541fbecb1df7069d77e35
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-11 02:59:54 +0200

    Added docstrings for utils module.

--- afthermal/util.py
+++ afthermal/util.py
@@ -1,4 +1,14 @@
 def in_range(low, high, step=None):
+    """Create a function that checks whether or not a value is in range.
+
+    :param low: Minimum valid value; value must be ``>= low``
+    :param high: Smallest invalid value; value must be ``< high``
+    :param step: Step size. Value must be a multipe of ``
+
+    :return: A function returning a boolean indicating whether or not the
+    value is valid, that is in the specified range and satisfying ``val = low +
+    k * step`` for an integer ``k``.
+    """
     def check(value):
         if not low <= value < high:
             return False
@@ -11,6 +21,18 @@ def in_range(low, high, step=None):
 
 
 def from_range(low, high, step=None, value_name='value'):
+    """Normalizes a value.
+
+    Will first check if the value is in range. If not, raises a
+    ``~exceptions.ValueError``.
+
+    :param low: See :func:`.in_range`.
+    :param high: See :func:`.in_range`.
+    :param step: See :func:`.in_range`.
+    :param value_name: Name for the value. Use in exception error message.
+    :return: ``(val - low) [/ step]``. Division by step only if step is not
+             ``None``.
+    """
     def convert(value):
         if not in_range(low, high, step)(value):
             msg = '{} not in range: {}; must be {} <= {} < {}'.format(
