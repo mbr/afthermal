386400c3cf3c88ecfa6b561ae4443bb4abff2fe4
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-29 14:46:53 +0200

    Added test-cases for text rendering.

---
+++ tests/test_text.py
@@ -0,0 +1,35 @@
+from afthermal.text import Text, ByteStringVisitor, Bold, Node
+
+import pytest
+
+
+@pytest.fixture
+def encoding():
+    return 'ascii'
+
+
+@pytest.fixture
+def bsv(encoding):
+    return ByteStringVisitor(encoding)
+
+
+def test_simple_text(bsv, encoding):
+    tx = Text(u'hello, world')
+    assert bsv.visit(tx) == u'hello, world'.encode(encoding)
+
+
+def test_simple_formatting(bsv, encoding):
+    tx = Text(u'hello')
+    fmtx = Bold(tx)
+
+    assert bsv.visit(fmtx) == (b'\x1B\x45\x01' + u'hello'.encode(encoding) +
+                               b'\x1B\x45\x00')
+
+
+def test_nested_formatting(bsv, encoding):
+    tx = Node(Bold(Bold(Text(u'hello')), Text(u'world')), Text(u'nonbold'))
+
+    assert bsv.visit(tx) == (
+        b'\x1B\x45\x01' + u'helloworld'.encode(encoding) + b'\x1B\x45\x00' +
+        'nonbold'.encode(encoding)
+    )
