5abadf58657e8e06923f191501fcb8edcaeefa8f
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-29 14:35:30 +0200

    First parts of new text rendering implementation.;

--- afthermal/text.py
+++ afthermal/text.py
@@ -13,6 +13,92 @@ from .hw import get_command
 # <dh>: double height
 # <del>: strike through
 
+
+class Visitor(object):
+    def visit(self, node):
+        for cls in type(node).mro():
+            meth = getattr(self, 'visit_' + cls.__name__, None)
+            if meth is None:
+                continue
+            return meth(node)
+
+        raise NotImplementedError('No visitation method visitor for {}'
+                                  .format(node.__class__.__name__))
+
+
+class Node(object):
+    def __init__(self, *children):
+        self.children = children
+
+
+class ByteStringVisitor(Visitor):
+    def __init__(self, encoding='ascii'):
+        super(ByteStringVisitor, self).__init__()
+        self.encoding = encoding
+
+    def visit_FormatMode(self, node):
+        buf = []
+        buf.append(node.SEQ_ON)
+
+        for child in node.children:
+            buf.append(self.visit(child))
+
+        buf.append(node.SEQ_OFF)
+
+        return b''.join(buf)
+
+    def visit_Text(self, node):
+        return node.text.encode(self.encoding)
+
+
+class FormatMode(Node):
+    def _enter(self, encoding):
+        raise NotImplementedError
+
+    def _exit(self, encoding):
+        raise NotImplementedError
+
+    def to_bytestring(self, encoding='ascii'):
+        buf = []
+        buf.append(self._enter(encoding))
+
+        for child in self.children:
+            buf.append(child.to_bytestring(encoding))
+
+        buf.append(self._exit(encoding))
+
+        return b''.join(buf)
+
+
+class Text(object):
+    def __init__(self, text):
+        self.text = text
+
+    def to_bytestring(self, encoding='ascii'):
+        # FIXME: escape special chars
+        return self.text.encode(encoding)
+
+
+class UpsideDown(FormatMode):
+    SEQ_ON = get_command('set_updown_mode', 1)
+    SEQ_OFF = get_command('set_updown_mode', 0)
+
+
+class Invert(FormatMode):
+    SEQ_ON = get_command('set_reverse_mode', 1)
+    SEQ_OFF = get_command('set_reverse_mode', 0)
+
+
+class Bold(FormatMode):
+    SEQ_ON = get_command('set_font_bold', 1)
+    SEQ_OFF = get_command('set_font_bold', 0)
+
+
+class Underline(FormatMode):
+    SEQ_ON = get_command('set_underline', 1)
+    SEQ_OFF = get_command('set_underline', 0)
+
+
 class Format(object):
     """Format text using printer specific escape sequences.
 
@@ -29,27 +115,6 @@ class Format(object):
                  strikethrough=False):
         start = b''
         end = b''
-
-        if strikethrough:
-            # turns off all other modes, so we need to set it first
-            start += get_command('set_print_mode', 1 << 6)
-            end = get_command('set_print_mode', 0) + end
-
-        if upside_down:
-            start += get_command('set_updown_mode', 1)
-            end = get_command('set_updown_mode', 0) + end
-
-        if invert:
-            start += get_command('set_reverse_mode', 1)
-            end = get_command('set_reverse_mode', 0) + end
-
-        if bold:
-            start += get_command('set_font_bold', 1)
-            end = get_command('set_font_bold', 0) + end
-
-        if underline:
-            start += get_command('set_underline', 1)
-            end = get_command('set_underline', 0) + end
 
         enlarge = 0
         if double_width:
